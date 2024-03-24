ef5480acccdbe0fa8e889a494ba4673e124a3a9c
Camila Palma <cpalma.contacto@gmail.com>
2024-03-24 00:48:44 -0300

    productos definidos

--- products.js
+++ products.js
@@ -2,33 +2,33 @@
 const productos = [
     {
         id: 1,
-        nombre: "harina",
-        precio: 50,
+        nombre: "Winona",
+        precio: 7,
         img:
-        "https://i.pinimg.com/474x/15/a8/9e/15a89e520c375b9ef995d4e5a4fc4052.jpg",
+        "https://i.pinimg.com/564x/de/7d/72/de7d725c21174a28a32e9929fe23f232.jpg",
     },
     {
         id: 2,
-        nombre: "galletas",
-         precio: 100,
-        img: "https://i.pinimg.com/474x/c9/74/77/c974779e0c81aca3a7998728b0072446.jpg",
+        nombre: "Meryl",
+         precio: 19,
+        img: "https://i.pinimg.com/564x/f3/57/02/f357028b7c5aad8de88fb536378bee9d.jpg",
     },
     {
         id: 3,
-        nombre: "jugo",
+        nombre: "Julia",
         precio: 150,
-        img: "https://i.pinimg.com/564x/4a/e4/3f/4ae43f1cea5b43e723103242135fc43b.jpg",
+        img: "https://i.pinimg.com/564x/fe/6d/31/fe6d3166f16f204c6a426232789e8d31.jpg",
     },
     {
         id: 4,
-        nombre: "leche",
+        nombre: "Charlize",
         precio: 200,
-        img: "https://i.pinimg.com/564x/cd/e3/85/cde385fa76d57a525dd764b20f615285.jpg",
+        img: "https://i.pinimg.com/564x/fc/e2/af/fce2af0c3b6123daf719f7240d52a701.jpg",
     },
     {
         id: 5,
-        nombre: "bebida",
+        nombre: "Angelina",
         precio: 250,
-        img: "https://i.pinimg.com/564x/26/e9/76/26e9766e8a74a840954affbf07519f1c.jpg",
+        img: "https://i.pinimg.com/564x/90/5e/7a/905e7ae8edebb0c2fc358477700ff95c.jpg",
     },
 ];
